6a14112a24c114edd7dc61aa213e4d0f418258f7
Mahmoud Fathy <mahmoudfathy.dev@gmail.com>
2026-01-25 21:04:38 +0200

    test

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/src/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h2 className="text-primary">hello world</h2>
+      <Button>hello</Button>
+    </main>
+  );
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div>
+            <div className="container">
+                hello from header
+            </div>
+        </div>
+    )
+}
+
+export default Header
